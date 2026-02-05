0b150f48dc073450a9730a81c74c1dc3a721dbb6
Pppham <kmitzel602@gmail.com>
2026-02-05 11:37:29 -0800

    cleaned gas dataframe

--- data.R
+++ data.R
@@ -4,11 +4,17 @@ library(tidyverse)
 library(here)
 
 
-#Datasets
+#Loading the Datasets
 
+#predicted temperature dataframe
 pred_temp_df <- read.csv(here("data", "Temp_GCMs.csv"))
+#natural gas consumption dataframe
 NC_gas_df <- read.csv(here("data", "NG_CONS_SUM_DCU_SNC_M.csv"))
-
+NC_gas_df <- NC_gas_df[-1, ] %>%
+  select(Back.to.Contents,
+         X,
+         X.1)
+#airport weather dataframes
 RDU_df <- read.csv(here("data", "USW00013722.csv"))
 GSO_df <- read.csv(here("data", "USW00013881.csv"))
 CLT_df <- read.csv(here("data", "USW00013881.csv"))
