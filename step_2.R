6e48580d0e7560ae0b91d2bcd442e6bf5c020184
Pppham <kmitzel602@gmail.com>
2026-02-05 17:57:34 -0800

    averaged airport data completed

--- step_2.R
+++ step_2.R
@@ -23,7 +23,15 @@ GSO_df_rename <- GSO_df %>%
 
 combined_df <- full_join(CLT_df_rename, GSO_df_rename, by = "DATE")
 
-combined_df <- full_join(airport_df, RDU_df_rename, by = "DATE")
+combined_df <- full_join(combined_df, RDU_df_rename, by = "DATE")
 
-
+airport_df <- combined_df %>%
+  rowwise() %>%
+  mutate(mean_CLDD = mean(c(CLDD_RDU, CLDD_CLT, CLDD_GSO), na.rm = TRUE),
+         mean_HTDD = mean(c(HTDD_RDU, HTDD_CLT, HTDD_GSO), na.rm = TRUE),
+         mean_TAVG = mean(c(TAVG_RDU, TAVG_CLT, TAVG_GSO), na.rm = TRUE),
+         mean_TMAX = mean(c(TMAX_RDU, TMAX_CLT, TMAX_GSO), na.rm = TRUE),
+         mean_TMIN = mean(c(TMIN_RDU, TMIN_CLT, TMIN_GSO), na.rm = TRUE)) %>%
+  ungroup() %>%
+  select(DATE, mean_CLDD, mean_HTDD, mean_TAVG, mean_TMAX, mean_TMIN)
 
